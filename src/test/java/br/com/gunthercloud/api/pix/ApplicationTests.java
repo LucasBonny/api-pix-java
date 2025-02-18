2cb81f8e7d37d447df7dab8364757fe59738b620
Lucas Bonny <lucas@gunthercloud.com.br>
2025-02-17 23:28:37 -0300

    Project created

---
+++ src/test/java/br/com/gunthercloud/api/pix/ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gunthercloud.api.pix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
